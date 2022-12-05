cf8038e102fb70d598ba6dc29719cc5202db5f14
Kwinten Fruytier <kwinten@thewave.tech>
2022-12-05 21:46:34 +0100

    adding fetchpriotiy high

--- src/html.js
+++ src/html.js
@@ -15,7 +15,12 @@ export default function HTML(props) {
                 <link rel="preconnect" href="https://www.googletagmanager.com" />
                 <link rel="preconnect" href="https://www.google-analytics.com" />
                 <link rel="preconnect" href="https://fast.wistia.net" />
-                <link rel="preload" as="image" href="../src/Images/homeHeroVideoPlaceholder.jpg" />
+                <link
+                    rel="preload"
+                    as="image"
+                    href="../src/Images/homeHeroVideoPlaceholder.jpg"
+                    fetchpriority="high"
+                />
                 <script
                     dangerouslySetInnerHTML={{
                         __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var
