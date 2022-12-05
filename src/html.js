81a5b0655c029e571470c7a7f24c0d8c7056130e
Kwinten Fruytier <kwinten@thewave.tech>
2022-12-05 21:43:50 +0100

    minor change to image link

--- src/html.js
+++ src/html.js
@@ -15,11 +15,7 @@ export default function HTML(props) {
                 <link rel="preconnect" href="https://www.googletagmanager.com" />
                 <link rel="preconnect" href="https://www.google-analytics.com" />
                 <link rel="preconnect" href="https://fast.wistia.net" />
-                <link
-                    rel="preload"
-                    as="image"
-                    href="../src/Images/homeHeroVideoPlaceholder.jpg"
-                ></link>
+                <link rel="preload" as="image" href="../src/Images/homeHeroVideoPlaceholder.jpg" />
                 <script
                     dangerouslySetInnerHTML={{
                         __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var
